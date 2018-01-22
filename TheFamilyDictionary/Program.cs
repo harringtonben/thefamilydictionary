a7bac13f87d47887ee58365f5526218874ca7a5c
Ben Harrington <harringtonben@icloud.com>
2018-01-22 13:16:01 -0600

    finished assignment

--- TheFamilyDictionary/Program.cs
+++ TheFamilyDictionary/Program.cs
@@ -10,6 +10,41 @@ namespace TheFamilyDictionary
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>()
+            {
+                {"name", "Barbara"},
+                {"age", "2"}
+            });
+
+            myFamily.Add("mother", new Dictionary<string, string>()
+            {
+                {"name", "Susan"},
+                {"age", "64"}
+            });
+
+            myFamily.Add("father", new Dictionary<string, string>()
+            {
+                {"name", "Michael"},
+                {"age", "67"}
+            });
+
+            foreach (var member in myFamily)
+            {
+                foreach (var person in member.Value)
+                {
+                    if (person.Key == "name")
+                    {
+                        Console.Write($"{person.Value} is my {member.Key} ");
+                    }
+                    else if (person.Key == "age")
+                    {
+                        Console.Write($"and is {person.Value} years old.");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
